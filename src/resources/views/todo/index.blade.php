e7cc9742d41e2741fd51717c93e9e9ea867cccf0
koichi0330 <get7446koichi@gmail.com>
2024-07-08 11:50:45 +0900

    refactor: コントローラで使用するTodoクラスインスタンスの生成と操作のリファクタリング。

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -10,7 +10,7 @@
         ToDo一覧
       </div>
       <div class="list-group list-group-flush">
-        @foreach ($todoList as $todo)
+        @foreach ($todos as $todo)
           <div class="d-flex align-items-center p-2">
             <span class="col-9">{{ $todo->content }}</span>
             <a href="{{ route('todo.show', $todo->id) }}" class="btn btn-info ml-3">詳細</a>
